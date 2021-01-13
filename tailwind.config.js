b69d0134d68ba60a23fa277325c838433567a5ab
Anand Mahesh <manand881@gmail.com>
2021-01-13 20:00:26 +0530

    reinit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,8 @@ module.exports = {
   purge: {
     enabled: true,
     content: [
-      "./public/**/*.html",
-      "./public/*.html",
-      "./src/**/*.js",
-      "./src/*.js",
-      "./src/**/*.html",
-      "./src/*.html",
-      "./public/**/*.js",
-      "./public/*.js",
+      "./**/*.html",
+      "./*.html"
     ],
     options: {
       whitelist: [],
